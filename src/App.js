b74cb94b759070f50a2897c1c6d5e6b53aa1d093
Ibnat Iqbal <ibbieibnat@gmail.com>
2021-05-22 15:48:36 +0600

    Adding Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./component/Home";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Home />
+      <Footer />
     </Router>
   );
 }
