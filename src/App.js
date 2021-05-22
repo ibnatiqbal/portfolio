ccd1b98b1a9694614ee4922f4bfb228de0542895
Ibnat Iqbal <ibbieibnat@gmail.com>
2021-05-22 12:38:23 +0600

    Basic Info Updated

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./component/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </Router>
   );
 }
