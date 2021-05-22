7606c8678e9cf9120e555ed4fbd898ae48763756
Ibnat Iqbal <ibbieibnat@gmail.com>
2021-05-22 12:15:32 +0600

    Nav Links Added

---
+++ src/component/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./css/style.css";
+import "./css/utilities.css";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <div className="container flex">
+          <h1 className="logo">Ibnat Iqbal</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/features">About Me</Link>
+              </li>
+              <li>
+                <Link to="/docs">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
