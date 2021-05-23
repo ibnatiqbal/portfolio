498aabf3dee66856d33f5eea7a849050ce779bc2
Ibnat Iqbal <ibbieibnat@gmail.com>
2021-05-23 13:11:44 +0600

    Contact details Updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./component/Home";
 import Footer from "./component/Footer";
+import Contact from "./component/Contact";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Home />
+      <Contact />
       <Footer />
     </Router>
   );
