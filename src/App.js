655aa2e5132baa32cd3035d627ca97f56bbef210
Ibnat Iqbal <ibbieibnat@gmail.com>
2021-05-25 12:55:32 +0600

    Added everything

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./component/Home";
+import Footer from "./component/Footer";
+import Contact from "./component/Contact";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
