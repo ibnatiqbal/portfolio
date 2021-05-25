a52f6670670d6a7cf6a7eb9520065699189ab25d
Ibnat Iqbal <ibbieibnat@gmail.com>
2021-05-25 12:17:51 +0600

    homepage added

---
+++ src/component/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <h2>About:</h2>
+      <div className="con">
+        <div className="content">
+          Lorem The href attribute requires a valid value to be accessible.
+          Provide a valid, navigable address as the href value. If you cannot
+          provide a valid href, but still need the element to resemble a link,
+          use a button and change it with appropriate styles.
+        </div>
+        <br />
+        <div className="content">
+          Lorem The href attribute requires a valid value to be accessible.
+          Provide a valid, navigable address as the href value. If you cannot
+          provide a valid href, but still need the element to resemble a link,
+          use a button and change it with appropriate styles.
+        </div>
+        <br />
+        <div className="content">
+          Lorem The href attribute requires a valid value to be accessible.
+          Provide a valid, navigable address as the href value. If you cannot
+          provide a valid href, but still need the element to resemble a link,
+          use a button and change it with appropriate styles.
+        </div>
+        <br />
+        <div className="content">
+          Lorem The href attribute requires a valid value to be accessible.
+          Provide a valid, navigable address as the href value. If you cannot
+          provide a valid href, but still need the element to resemble a link,
+          use a button and change it with appropriate styles.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
